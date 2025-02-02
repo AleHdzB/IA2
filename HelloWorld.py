7553730d42200c16ba3fa42b036b184cd5169a20
unknown <alejandro.hernandez4819@alumnos.udg.mx>
2025-02-01 20:47:24 -0600

    Add: Grafics for OR, AND, XOR Gates

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,61 +3,78 @@ import matplotlib.pyplot as plt
 
 class Perceptron:
     def __init__(self, n_inputs):
-        self.w = -1 + 2 *np.random.rand(n_inputs)
-        self.b = -1 + 2 *np.random.rand()
+        self.w = -1 + 2 * np.random.rand(n_inputs)
+        self.b = -1 + 2 * np.random.rand()
 
-    def predict(self , X):
+    def predict(self, X):
         _, p = X.shape
         Y_est = np.zeros(p)
         for i in range(p):
-            y_est = np.dot(self.w,X[:,i]) + self.b
+            y_est = np.dot(self.w, X[:, i]) + self.b
             if y_est >= 0:
                 Y_est[i] = 1
             else:
                 Y_est[i] = 0
         return Y_est
-        
-    def fit(self, X, Y, epochs = 100, lr = 0.1):
+
+    def fit(self, X, Y, epochs=100, lr=0.1):
         _, p = X.shape
         for _ in range(epochs):
             for i in range(p):
-                y_est = self.predict(X[:,i].reshape(-1,1))
-                self.w += lr * (Y[i] - y_est) * X[:,i]
+                y_est = self.predict(X[:, i].reshape(-1, 1))
+                self.w += lr * (Y[i] - y_est) * X[:, i]
                 self.b += lr * (Y[i] - y_est)
 
+# Función para dibujar el perceptrón
+def draw_perceptron(model, ax):
+    w1, w2, b = model.w[0], model.w[1], model.b
+    ax.plot([-2, 2], [(1/w2)*(-w1*(-2) - b), (1/w2)*(-w1*(2) - b)], '--k')
 
-# Ejemplo
+# Datos de entrada (las mismas para todas las compuertas)
+X = np.array([[0, 0, 1, 1], [0, 1, 0, 1]])
 
-def draw_perceptron(model):
-    w1,w2,b  = model.w[0], model.w[1], model.b
-    plt.plot([-2,2], [(1/w2)*(-w1*(-2) - b), (1/w2)*(-w1*(2) - b)], '--k')
+# Etiquetas para las compuertas AND, OR y XOR
+Y_and = np.array([0, 0, 0, 1])  # Compuerta AND
+Y_or = np.array([0, 1, 1, 1])   # Compuerta OR
+Y_xor = np.array([0, 1, 1, 0])  # Compuerta XOR
 
+# Crear y entrenar los perceptrones
+model_and = Perceptron(2)
+model_and.fit(X, Y_and)
 
-model = Perceptron(2)
+model_or = Perceptron(2)
+model_or.fit(X, Y_or)
 
-#Datos
+model_xor = Perceptron(2)
+model_xor.fit(X, Y_xor)
 
-X = np.array([[0,0,1,1],[0,1,0,1]])
-# Y = np.array([0,1,1,1])  #OR gate
-# Y = np.array([0,0,0,1]) #AND gate
-Y = np.array([0,1,1,0]) #XOR gate  (no linealmente separable)
+# Función para graficar los resultados
+def plot_results(X, Y, model, title, ax):
+    _, p = X.shape
+    for i in range(p):
+        if Y[i] == 0:
+            ax.plot(X[0, i], X[1, i], 'or')
+        else:
+            ax.plot(X[0, i], X[1, i], 'ob')
+    draw_perceptron(model, ax)
+    ax.set_title(title)
+    ax.grid(True)
+    ax.set_xlim([-2, 2])
+    ax.set_ylim([-2, 2])
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
 
-model.fit(X,Y)
+# Crear una figura con 3 subgráficos
+fig, axs = plt.subplots(1, 3, figsize=(15, 5))
 
-#Dibujo
+# Graficar AND
+plot_results(X, Y_and, model_and, 'Perceptrón - Compuerta AND', axs[0])
 
-_, p = X.shape
-for i in range(p):
-    if Y[i] == 0:
-        plt.plot(X[0,i],X[1,i],'or')
-    else:
-        plt.plot(X[0,i],X[1,i],'ob')
-        
-plt.title('Perceptron')
-plt.grid('on')
-plt.xlim([-2,2])
-plt.ylim([-2,2])
-plt.xlabel('x1')
-plt.ylabel('x2')   
-draw_perceptron(model)
+# Graficar OR
+plot_results(X, Y_or, model_or, 'Perceptrón - Compuerta OR', axs[1])
+
+# Graficar XOR
+plot_results(X, Y_xor, model_xor, 'Perceptrón - Compuerta XOR', axs[2])
+
+plt.tight_layout()
 plt.show()
